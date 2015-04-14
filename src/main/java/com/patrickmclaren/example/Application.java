8b163c2ec96289661c24ec0995515e466bc2afe8
Patrick McLaren <patrickkmclaren@gmail.com>
2015-04-14 14:32:40 -0400

    cleaning imports

--- src/main/java/com/patrickmclaren/example/Application.java
+++ src/main/java/com/patrickmclaren/example/Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.EntityManagerFactory;
--- src/main/java/com/patrickmclaren/example/user/UserRepository.java
+++ src/main/java/com/patrickmclaren/example/user/UserRepository.java
@@ -1,9 +1,6 @@
 package com.patrickmclaren.example.user;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
